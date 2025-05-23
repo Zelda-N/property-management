24ac44029f20e102e12b11d9935d56dc8ec6fb60
DA M1 78 <dam178@MacBookAir.lan>
2025-05-23 21:40:45 +0200

    logging

--- src/main/java/com/mycompany/property_management/dto/UserDTO.java
+++ src/main/java/com/mycompany/property_management/dto/UserDTO.java
@@ -3,6 +3,9 @@ package com.mycompany.property_management.dto;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,8 +23,14 @@ public class UserDTO {
 
     private Long id;
     private String ownerName;
+    @NotNull(message = "Owner Email is compulsory")
+    @Size(min = 1, max = 50, message = "Owner Email should be between 1 to 50 characters")
+    @NotEmpty(message = "Owner Email cannot be empty")
     private String ownerEmail;
     private String phone;
+    @NotNull(message = "Password cannot be null")
+    @NotEmpty(message = "Password cannot be empty")
+    @Size(min = 6, max = 13, message = "Password should contain between 6 to 13 characters")
     private String password;
 
 
